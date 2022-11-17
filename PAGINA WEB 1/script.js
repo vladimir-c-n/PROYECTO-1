406fd1458f8db0c1d457bf4bfe2a4622a1dee3b2
VLADIMIR CALCINA N <117945697+vladimir-c-n@users.noreply.github.com>
2022-11-17 10:06:28 -0400

    Add files via upload

---
+++ PAGINA WEB 1/script.js
@@ -0,0 +1,20 @@
+$(document).ready(function(){
+
+    $("#menu").click(function(){
+        $(this).toggleClass("fa-times");
+        $(".navbar").toggleClass("nav-toggle");
+    });
+
+    $(window).on("load scroll",function(){
+
+        $("#menu").removeClass("fa-times");
+        $(".navbar").removeClass("nav-toggle");
+        
+        if($(window).scrollTop() > 0){
+            $("header").addClass("sticky");
+        }else{
+            $("header").removeClass("sticky");
+        }
+    });
+
+});
